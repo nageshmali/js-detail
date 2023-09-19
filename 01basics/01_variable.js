aadb781574383f81a9030f026f74d5e08d769526
Nagesh Mali <malinagesh60@gmail.com>
2023-09-19 17:45:02 +0530

    done with variables and datatypes

---
+++ 01basics/01_variable.js
@@ -0,0 +1,21 @@
+
+//3 types
+const accountID = 1234;
+
+var accountName = "Nagesh";
+
+let accountLog = true;
+
+//Scoping and details
+
+/*
+1. const - once declared cannot be reassigned - Local Scope
+2. var - Global Scope
+3. let - Local Scope
+
+Note - avoid using var to eliminate scoping issues
+*/
+
+console.log(accountID);
+console.log(accountName);
+console.log(accountLog);
---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,23 @@
+
+//PRIMITIVE
+/*
+1. Numbers - 321,53 etc
+2. BigInt - very large number
+3. String - "nagesh"
+4. Boolean - true/false
+5. undefined
+6. null
+*/
+
+//NON - PRIMITIVE
+
+/*
+1. Objects
+     const name = {
+        key : value
+     }
+
+
+2. Array
+   name = [value,value...]
+*/
